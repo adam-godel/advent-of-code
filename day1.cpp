fd170c778493f696c691a6df65c7afc15993386a
adam-godel <agodel@bu.edu>
2024-12-07 09:38:02 -0500

    Updated formatting

--- day1.cpp
+++ day1.cpp
@@ -6,68 +6,48 @@
 #include <cmath>
 using namespace std;
 
-int part1(vector<int> left, vector<int> right)
-{
+int part1(vector<int> left, vector<int> right) {
     int result = 0;
-    for (int i = 0; i < left.size(); i++)
-    {
+    for (int i = 0; i < left.size(); i++) {
         result += abs(left.at(i) - right.at(i));
     }
     return result;
 }
 
-int part2(vector<int> left, vector<int> right)
-{
+int part2(vector<int> left, vector<int> right) {
     int i = 0, j = 0, k = 0, curEqual = 0, result = 0;
-    while (i < left.size() && j < right.size())
-    {
-        if (left.at(i) < right.at(j))
-        {
-            if (curEqual == left.at(i))
-            {
+    while (i < left.size() && j < right.size()) {
+        if (left.at(i) < right.at(j)) {
+            if (curEqual == left.at(i)) {
                 result += k * curEqual;
-            }
-            else
-            {
+            } else {
                 k = 0;
             }
             i++;
-        }
-        else if (left.at(i) > right.at(j))
-        {
-            if (curEqual == right.at(j))
-            {
+        } else if (left.at(i) > right.at(j)) {
+            if (curEqual == right.at(j)) {
                 result += k * curEqual;
-            }
-            else
-            {
+            } else {
                 k = 0;
             }
             j++;
-        }
-        else
-        {
-            if (left.at(i) != curEqual)
-            {
+        } else {
+            if (left.at(i) != curEqual) {
                 k = 0;
             }
             result += (2 * k + 1) * left.at(i);
             curEqual = left.at(i);
-            i++;
-            j++;
-            k++;
+            i++; j++; k++;
         }
     }
     return result;
 }
 
-int main()
-{
+int main() {
     string line;
     ifstream input("day1.txt");
     vector<int> left, right;
-    while (getline(input, line))
-    {
+    while (getline(input, line)) {
         stringstream s(line);
         int leftNum, rightNum;
         s >> leftNum >> rightNum;
